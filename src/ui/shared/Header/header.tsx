d09e752c7985d7b3dd25d4d07bff3a35d6e823ed
Roland Labrador <rolando1515@hotmail.com>
2023-04-11 16:04:24 -0400

    New notes

--- src/ui/shared/Header/header.tsx
+++ src/ui/shared/Header/header.tsx
@@ -1,11 +1,17 @@
 import { ListFormat } from 'typescript';
 import { Style } from './styles';
 
+// this is the function for the header component. 
+// it returns the header component with the styles from the styles.tsx file.  (this is where you put the css styling)
+
+
 export function Header() {
   return (
     <Style.Container>
       <div className="header-content">
-         <img src="/images/logo.svg" alt="logo" />
+        <span className="header-logo">
+          R.L
+        </span>
          <ul>
          <li>Projects</li>
          <li>Resume</li>
--- src/ui/shared/Header/styles.tsx
+++ src/ui/shared/Header/styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// To make it easier go to figma and copy the styles from there. 
 export const Style = {
   Container: styled.header`
     height: 123px;
@@ -13,6 +14,17 @@ export const Style = {
       max-width: var(--max-width);
       height: 100%;
       border: 1px solid red;
+   
+      .header-logo {
+      width: 86px;
+      height: 64px;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 300;
+      font-size: 53.0547px;
+      line-height: 64px;
+      color: #FFFFFF;
+      }
     }
   `,
 };
