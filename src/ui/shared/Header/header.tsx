1ad4551731e37701f944b868fc50b5c5b39eac6e
Roland Labrador <rolando1515@hotmail.com>
2023-04-11 13:33:04 -0400

    Changes

--- src/ui/shared/Header/header.tsx
+++ src/ui/shared/Header/header.tsx
@@ -1,9 +1,19 @@
+import { ListFormat } from 'typescript';
 import { Style } from './styles';
 
 export function Header() {
   return (
     <Style.Container>
-      <div className="header-content"></div>
+      <div className="header-content">
+         <img src="/images/logo.svg" alt="logo" />
+         <ul>
+         <li>Projects</li>
+         <li>Resume</li>
+         <li>Contact</li>
+         </ul>
+       </div>
+   
     </Style.Container>
   );
 }
+
