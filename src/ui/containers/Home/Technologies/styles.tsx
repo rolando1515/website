669f70e2835d2b4a163e8294c2882760822f6210
Roland Labrador <rolando1515@hotmail.com>
2023-04-23 12:25:48 -0400

    Resume

--- src/ui/containers/Home/Technologies/styles.tsx
+++ src/ui/containers/Home/Technologies/styles.tsx
@@ -19,6 +19,75 @@ export const Style = {
       margin-bottom: 25px;
     }
 
+    h3 {font-family: 'Inter';
+      font-style: normal;
+      font-weight: 300;
+      font-size: 18px;
+      line-height: 22px;
+      color: #ffffff;
+      margin-bottom: 25px;
+      margin-left: 200px;
+    }
+
+
+      .resume {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 316px;
+  height: 29px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 29px;
+  color: #FFFFFF;
+}
+
+.name {
+  margin-top: 10px;
+}
+
+.contact {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.section {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.section h2 {
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  margin-bottom: 10px;
+}
+
+.section ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.section ul li {
+  margin-bottom: 10px;
+}
+
+.section ul li h3 {
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  margin-bottom: 5px;
+}
+
+.section ul li p {
+  font-size: 14px;
+  line-height: 17px;
+  margin: 0;
+  padding: 0;
+}
 
 
 
--- src/ui/containers/Home/Technologies/technologies.tsx
+++ src/ui/containers/Home/Technologies/technologies.tsx
@@ -3,7 +3,73 @@ import { Style } from './styles';
 export function Technologies() {
   return (
     <Style.Container>
-      <h2>Resume </h2>
+    <div>
+    <h2>Experience</h2> 
+    </div>
+    <div>    <h3>Name</h3>    </div>
+    
+   
+
+
+      
     </Style.Container>
   );
 }
+
+
+
+//<h2>Resume </h2>
+
+/*
+  <h3>Name</h3>
+  <p>John Doe</p>
+</div>
+<div>
+  <h3>Contact</h3>
+  <p>1234 Main St.</p>
+  <p>Anytown, USA 12345</p>
+  <p>email@example.com</p>
+  <p>555-555-5555</p>
+</div>
+<div>
+  <h3>Experience</h3>
+  <ul>
+    <li>
+      <h4>Job Title</h4>
+      <p>Employer Name</p>
+      <p>Date Range</p>
+      <ul>
+        <li>Responsibility 1</li>
+        <li>Responsibility 2</li>
+        <li>Responsibility 3</li>
+      </ul>
+    </li>
+    <li>
+      <h4>Job Title</h4>
+      <p>Employer Name</p>
+      <p>Date Range</p>
+      <ul>
+        <li>Responsibility 1</li>
+        <li>Responsibility 2</li>
+        <li>Responsibility 3</li>
+      </ul>
+    </li>
+  </ul>
+</div>
+<div>
+  <h3>Education</h3>
+  <ul>
+    <li>
+      <h4>Degree Title</h4>
+      <p>University Name</p>
+      <p>Date Range</p>
+    </li>
+    <li>
+      <h4>Degree Title</h4>
+      <p>University Name</p>
+      <p>Date Range</p>
+    </li>
+  </ul>
+</div>
+</div>
+*/
