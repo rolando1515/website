c02175e084b3a06110776f74eb61a227f07307c4
Momoh Nobert <okhai0112@gmail.com>
2023-05-02 13:59:45 +0100

    adjustments to featured project

--- src/ui/components/ProjectCard/projectCard.tsx
+++ src/ui/components/ProjectCard/projectCard.tsx
@@ -1,43 +1,25 @@
 import { Github, Link as LinkIcon } from '../Icons';
 import { Style } from './styles';
 
-export function ProjectCard() {
+export function ProjectCard({
+  title,
+  detail,
+}: {
+  title: string;
+  detail: string;
+}) {
   return (
     <Style.Container>
       <figure></figure>
       <hgroup>
-        <h3>Vroom Stock App</h3>
+        <h3>{title}</h3>
         <div>
           <Github />
           <LinkIcon />
         </div>
       </hgroup>
-     
-      <span>
-      Python-based Stock Price Web Application. Using Streamlit and yfinance libraries, 
-      this app fetches company information and real-time stock prices for S&P 500 companies.
-      With less than 50 lines of code.
-      </span>
-    </Style.Container>
-  );
-}
 
-export function ProjectCard1() {
-  return (
-    <Style.Container>
-      <figure></figure>
-      <hgroup>
-        <h3>To do list</h3>
-        <div>
-          <Github />
-          <LinkIcon />
-        </div>
-      </hgroup>
-     
-      <span>
-      A simple to do list app made with HTML,CSS and JavaScript. 
-      It's fast, responsive, and uses local storage to keep data secure.
-      </span>
+      <p>{detail}</p>
     </Style.Container>
   );
 }
--- src/ui/containers/Home/FeaturedProjects/featuredProjects.tsx
+++ src/ui/containers/Home/FeaturedProjects/featuredProjects.tsx
@@ -1,14 +1,27 @@
 import { ProjectCard } from 'ui/components';
 import { Style } from './styles';
-import { ProjectCard1 } from 'ui/components/ProjectCard/projectCard';
+
+const project_data = [
+  {
+    title: 'Vroom Stock App',
+    detail:
+      'Python-based Stock Price Web Application. Using Streamlit and yfinance libraries, this app fetches company information and real-time stock prices for S&P 500 companies. With less than 50 lines of code.',
+  },
+  {
+    title: 'To do list',
+    detail:
+      'A simple to do list app made with HTML,CSS and JavaScript. Its fast, responsive, and uses local storage to keep data secure.',
+  },
+];
 
 export function FeaturedProjects() {
   return (
     <Style.Container>
       <h2>FEATURED PROJECTS</h2>
       <div className="projects">
-        <ProjectCard />
-        <ProjectCard1 />
+        {project_data.map(({ detail, title }) => (
+          <ProjectCard key={title} detail={detail} title={title} />
+        ))}
       </div>
     </Style.Container>
   );
