c823a0422ff9a048980c5deb8634dc8ebc3da81d
Roland Labrador <rolando1515@hotmail.com>
2023-06-06 08:28:02 -0400

    comit

--- src/ui/containers/Home/FeaturedProjects/featuredProjects.tsx
+++ src/ui/containers/Home/FeaturedProjects/featuredProjects.tsx
@@ -5,7 +5,7 @@ const project_data = [
   {
     title: 'Vroom Stock App',
     image: '/project_pictures/vroomstockapp.png',
-    github: '',
+    github: 'https://github.com/rolando1515/vroomstockpriceapp',
     link: 'https://rolando1515-vroomstockpriceapp-myapp-s6gjuc.streamlit.app/',
     detail:
       'Python-based Stock Price Web Application. Using Streamlit and yfinance libraries, this app fetches company information and real-time stock prices for S&P 500 companies. With less than 50 lines of code.',
@@ -13,7 +13,7 @@ const project_data = [
   {
     title: 'To do list',
     image: '/project_pictures/taskList.png',
-    github: '',
+    github: 'https://github.com/rolando1515/To-do-list',
     link: 'https://to-do-list-r4qd645pj-rolando1515.vercel.app/',
     detail:
       'A simple to do list app made with HTML,CSS and JavaScript. Its fast, responsive, and uses local storage to keep data secure.',
