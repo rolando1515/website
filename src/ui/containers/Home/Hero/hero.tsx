cb35589b246032815a63ca9fd0c17f901d9390f5
Roland Labrador <rolando1515@hotmail.com>
2023-06-16 17:33:17 -0400

    commit

--- src/ui/containers/Home/Hero/hero.tsx
+++ src/ui/containers/Home/Hero/hero.tsx
@@ -7,13 +7,7 @@ export function Hero() {
       <span>Hey, I'm</span>
       <h1>Rolando Labrador</h1>
       <p>
-        I'm a software engineer based in Toronto, Canada and also a
-        communication and journalism student. I enjoy creating things that live
-        on the internet, whether that be websites, applications, or anything in
-        between. I have been freelancing for a year now while studying at the
-        university and I've manage to gain a decent amount of experience and
-        valuable knowledge from all different kinds of fields throughout my
-        projects/work.
+        I'm a QA engineer based in South Florida......blah blah blah blah blah
       </p>
       <button>
         Say hi <RightArrow />
