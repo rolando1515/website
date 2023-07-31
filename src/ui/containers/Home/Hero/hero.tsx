05790b0d74924e1096e6310739ef0b154818504c
Roland Labrador <rolando1515@hotmail.com>
2023-07-31 19:25:45 -0400

    commit

--- src/ui/containers/Home/Hero/hero.tsx
+++ src/ui/containers/Home/Hero/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
       <span>Hey, I'm</span>
       <h1>Rolando Labrador</h1>
       <p>
-        I'm a QA engineer based in South Florida......blah blah blah blah blah
+        I'm a QA engineer based in South Florida.
       </p>
       <button>
          <RightArrow />
